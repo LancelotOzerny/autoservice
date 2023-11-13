41f50de4f20167b70e894cace1693e097a477fe0
Максим Беляков <belyakov@intervolga.ru>
2023-11-13 08:58:38 +0300

    [KP-9] Переделал галерею под flex и более плавный вид

--- typescript/gallery.ts
+++ typescript/gallery.ts
@@ -1,22 +1,10 @@
 let images = document.getElementsByClassName('gallery-photo--wrapper') as HTMLCollectionOf<HTMLElement>;
-let galleryWrapper : Element = document.getElementsByClassName('gallery-wrapper')[0];
 let currentImage: number = 0;
 
 for (let i = 0; i < images.length; ++i) {
     images[i].addEventListener('click', () => {
-        images[currentImage].style.gridRow = "auto";
-        images[currentImage].style.gridColumn = "auto";
-
+        images[currentImage].style.width = "33.333%";
         currentImage = i;
-
-        images[currentImage].style.gridRow = "1/3";
-        images[currentImage].style.gridColumn = "1/3";
-
-        setTimeout(scrollToSlides, 200);
+        images[currentImage].style.width = "66.666%";
     });
-}
-
-function scrollToSlides() : void
-{
-    galleryWrapper.scrollIntoView({behavior: 'smooth', block: 'start'});
 }

d4f77718f5b57d2440ed6212e1b5157c38d832a4
Максим Беляков <belyakov@intervolga.ru>
2023-11-13 08:07:02 +0300

    [KP-9] реализовал спагетти-скрипт слайдера

---
+++ typescript/slider.ts
@@ -0,0 +1,71 @@
+let slides: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName('slide') as HTMLCollectionOf<HTMLElement>;
+let slideLeft: HTMLElement = document.getElementById("left-slider-arrow");
+let slideRight: HTMLElement = document.getElementById("right-slider-arrow");
+let currentSlideIndex: number = 0;
+let lastSlide: number = 0;
+let slideActive : boolean = false;
+
+slideLeft.addEventListener("click", SlideLeft);
+slideRight.addEventListener("click", SlideRight);
+
+function SlideRight() : void
+{
+    if (slideActive)
+    {
+        return;
+    }
+    slideActive = true;
+
+    let lastSlide: number = currentSlideIndex;
+
+    if (--currentSlideIndex < 0)
+    {
+        currentSlideIndex = slides.length - 1;
+    }
+
+    slides[currentSlideIndex].style.zIndex = "-1";
+    slides[lastSlide].style.left = "100%";
+    slides[lastSlide].style.opacity = "0";
+
+    setTimeout(() => {
+        slides[currentSlideIndex].style.zIndex = "-1";
+        slides[lastSlide].style.zIndex = "-5";
+
+        setTimeout(() => {
+            slides[lastSlide].style.left = "0";
+            slides[lastSlide].style.opacity = "1";
+            slideActive = false;
+        }, 50);
+    }, 500);
+}
+
+function SlideLeft() : void
+{
+    if (slideActive)
+    {
+        return;
+    }
+    slideActive = true;
+
+    lastSlide = currentSlideIndex;
+
+    if (++currentSlideIndex >= slides.length)
+    {
+        currentSlideIndex = 0;
+    }
+
+    slides[currentSlideIndex].style.zIndex = "-1";
+    slides[lastSlide].style.left = "-100%";
+    slides[lastSlide].style.opacity = "0";
+
+    setTimeout(() => {
+        slides[currentSlideIndex].style.zIndex = "-1";
+        slides[lastSlide].style.zIndex = "-5";
+
+        setTimeout(() => {
+            slides[lastSlide].style.left = "0";
+            slides[lastSlide].style.opacity = "1";
+            slideActive = false;
+        }, 50);
+    }, 500);
+}
